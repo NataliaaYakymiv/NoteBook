c65256c04e176c5dd19a415d346726b951500550
yagudron <andrii.konkov@gmail.com>
2017-02-21 16:14:55 +0200

    notes page reworked

--- NoteBook/NoteBook/Pages/NotePage.xaml.cs
+++ NoteBook/NoteBook/Pages/NotePage.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using NoteBook.Servises;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace NoteBook.Pages
 {
@@ -11,24 +12,7 @@ namespace NoteBook.Pages
         protected async override void OnAppearing()
         {
             Temp = await App.NotesItemManager.GetTasksAsync().ConfigureAwait(true);
-            //notesList.ItemsSource = await NoteBook.NotesItemManager.GetTasksAsync().ConfigureAwait(false);
             notesList.ItemsSource = Temp;
-            //base.OnAppearing();
-
-            //if (Constants.RestUrl.Contains("developer.xamarin.com"))
-            //{
-            //    if (!alertShown)
-            //    {
-            //        await DisplayAlert(
-            //            "Hosted Back-End",
-            //            "This app is running against Xamarin's read-only REST service. To create, edit, and delete data you must update the service endpoint to point to your own hosted REST service.",
-            //            "OK");
-            //        alertShown = true;
-            //    }
-            //}
-
-            //notesList.ItemsSource = Notes;
-            //notesList.ItemsSource = await App.NotesItemManager.GetTasksAsync().ConfigureAwait(false);
             base.OnAppearing();
         }
 
@@ -47,25 +31,22 @@ namespace NoteBook.Pages
         public NotePage()
         {
             InitializeComponent();
+            ForceLayout();
         }
+
 
         void NotesList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            //selected.Text = ((NoteModel)e.SelectedItem).NoteName;
-        }
+            UpdateButton.IsEnabled = true;
 
-        //private async void OnCreate(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new CreateNotePage());
-        //}
-        //private async void OnUpdate(object sender, EventArgs e)
-        //{
-        //    await Navigation.PushAsync(new UpdateNotePage());
-        //}
-        //private async void OnDelete(object sender, EventArgs e)
-        //{
-        //    throw new Exception();
-        //}
+            //if (notesList.SelectedItem != null)
+            //{
+            //}
+            //else
+            //{
+            //    notesList.
+            //}
+        }
 
         private async void OnCreate(object sender, EventArgs e)
         {
@@ -73,7 +54,6 @@ namespace NoteBook.Pages
         }
         private async void OnUpdate(object sender, EventArgs e)
         {
-            //await Navigation.PushAsync(new UpdateNotePage());
             await Navigation.PushAsync(new UpdateNotePage((NoteModel)notesList.SelectedItem));
 
         }
@@ -83,12 +63,5 @@ namespace NoteBook.Pages
             var response = NotesService.GetService().Delete((NoteModel)notesList.SelectedItem);
             OnAppearing();
         }
-
-        private async void OnRefresh(object sender, EventArgs e)
-        {
-            OnAppearing();
-            //notesList.ItemsSource = await App.NotesItemManager.GetTasksAsync().ConfigureAwait(false);
-        }
-
     }
 }
